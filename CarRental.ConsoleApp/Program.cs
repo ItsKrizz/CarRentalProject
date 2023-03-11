2851799e4629d88d623ae2d85c8356ff29c5d55e
Kristiyan <krisiarnaudov12@gmail.com>
2023-03-11 19:23:20 +0200

    services changed with using

--- CarRental.ConsoleApp/Program.cs
+++ CarRental.ConsoleApp/Program.cs
@@ -9,74 +9,65 @@ namespace CarRental.ConsoleApp
     {
         static void Main(string[] args)
         {
-            // Create a new instance of the AppDbContext
             using (var context = new AppDbContext())
             {
-                // Create a new instance of each service, passing in the AppDbContext
-                var vehicleService = new VehicleService();
+                // Adding a new customer
                 var customerService = new CustomerService();
+                var customer = customerService.AddCustomer("John Doe", "johndoe@example.com", "1234567890");
+                Console.WriteLine($"Customer with ID {customer.Id} has been added.");
+
+                // Adding a new location
                 var locationService = new LocationService();
+                var location = locationService.AddLocation("123 Main St");
+                Console.WriteLine($"Location with ID {location.Id} has been added.");
+
+                // Adding a new vehicle
+                var vehicleService = new VehicleService();
+                var vehicle = vehicleService.AddVehicle("Toyota", "Camry", 2021);
+                Console.WriteLine($"Vehicle with ID {vehicle.Id} has been added.");
+
+                // Adding a new reservation
                 var reservationService = new ReservationService();
+                var startDate = DateTime.Today.AddDays(1);
+                var endDate = DateTime.Today.AddDays(7);
+                var reservation = reservationService.AddReservation(startDate, endDate, vehicle.Id, customer.Id);
+                Console.WriteLine($"Reservation with ID {reservation.Id} has been added.");
 
-                // Use the services to add data to the database
-                var vehicle = vehicleService.AddVehicle("Toyota", "Camry", 2020);
-                var customer = customerService.AddCustomer("John Doe", "johndoe@email.com", "555-555-5555");
-                var location = locationService.AddLocation("123 Main St.");
-                var reservation = reservationService.AddReservation(DateTime.Now, DateTime.Now.AddDays(7), vehicle.Id, customer.Id);
+                // Retrieving a customer by ID
+                var retrievedCustomer = customerService.GetCustomerById(customer.Id);
+                Console.WriteLine($"Retrieved customer: {retrievedCustomer.Name}");
 
-                // Use the services to retrieve data from the database
-                var allVehicles = vehicleService.GetAllVehicles();
+                // Retrieving all customers
                 var allCustomers = customerService.GetAllCustomers();
+                Console.WriteLine($"Number of customers: {allCustomers.Count}");
+
+                // Retrieving a location by ID
+                var retrievedLocation = locationService.GetLocationById(location.Id);
+                Console.WriteLine($"Retrieved location: {retrievedLocation.Address}");
+
+                // Retrieving all locations
                 var allLocations = locationService.GetAllLocations();
-                var reservationById = reservationService.GetReservationById(reservation.Id);
-                var reservationsByCustomerId = reservationService.GetReservationsByCustomerId(customer.Id);
-                var reservationsByVehicleId = reservationService.GetReservationsByVehicleId(vehicle.Id);
+                Console.WriteLine($"Number of locations: {allLocations.Count}");
 
-                // Output the data retrieved from the database
-                Console.WriteLine("All Vehicles:");
-                foreach (var v in allVehicles)
-                {
-                    Console.WriteLine($"ID: {v.Id}, Make: {v.Make}, Model: {v.Model}, Year: {v.Year}");
-                }
+                // Retrieving a vehicle by ID
+                var retrievedVehicle = vehicleService.GetVehicleById(vehicle.Id);
+                Console.WriteLine($"Retrieved vehicle: {retrievedVehicle.Make} {retrievedVehicle.Model}");
 
-                Console.WriteLine();
+                // Retrieving all vehicles
+                var allVehicles = vehicleService.GetAllVehicles();
+                Console.WriteLine($"Number of vehicles: {allVehicles.Count}");
 
-                Console.WriteLine("All Customers:");
-                foreach (var c in allCustomers)
-                {
-                    Console.WriteLine($"ID: {c.Id}, Name: {c.Name}, Email: {c.Email}, Phone Number: {c.PhoneNumber}");
-                }
+                // Retrieving all reservations for a customer
+                var customerReservations = reservationService.GetReservationsByCustomerId(customer.Id);
+                Console.WriteLine($"Number of reservations for customer: {customerReservations.Count}");
 
-                Console.WriteLine();
+                // Retrieving all reservations for a vehicle
+                var vehicleReservations = reservationService.GetReservationsByVehicleId(vehicle.Id);
+                Console.WriteLine($"Number of reservations for vehicle: {vehicleReservations.Count}");
 
-                Console.WriteLine("All Locations:");
-                foreach (var l in allLocations)
-                {
-                    Console.WriteLine($"ID: {l.Id}, Address: {l.Address}");
-                }
-
-                Console.WriteLine();
-
-                Console.WriteLine($"Reservation with ID {reservationById.Id}:");
-                Console.WriteLine($"Start Date: {reservationById.StartDate}, End Date: {reservationById.EndDate}");
-                Console.WriteLine($"Vehicle: {reservationById.Vehicle.Make} {reservationById.Vehicle.Model} ({reservationById.Vehicle.Year})");
-                Console.WriteLine($"Customer: {reservationById.Customer.Name} ({reservationById.Customer.Email}, {reservationById.Customer.PhoneNumber})");
-
-                Console.WriteLine();
-
-                Console.WriteLine($"Reservations for customer {customer.Name}:");
-                foreach (var r in reservationsByCustomerId)
-                {
-                    Console.WriteLine($"ID: {r.Id}, Vehicle: {r.Vehicle.Make} {r.Vehicle.Model}, Start Date: {r.StartDate}, End Date: {r.EndDate}");
-                }
-
-                Console.WriteLine();
-
-                Console.WriteLine($"Reservations for vehicle {vehicle.Make} {vehicle.Model}:");
-                foreach (var r in reservationsByVehicleId)
-                {
-                    Console.WriteLine($"ID: {r.Id}, Customer: {r.Customer.Name} ({r.Customer.Email}, {r.Customer.PhoneNumber}), Start Date: {r.StartDate}, End Date: {r.EndDate}");
-                }
+                // Retrieving all available vehicles for a given date range
+                var availableVehicles = vehicleService.GetAvailableVehicles(startDate, endDate);
+                Console.WriteLine($"Number of available vehicles: {availableVehicles.Count}");
             }
         }
     }
