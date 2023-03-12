4c9b0fcd3a34497460ab63635ade53ff3dda25f3
LMilenov <lusi_milenov@abv.bg>
2023-03-12 14:45:54 +0200

    Created Form

---
+++ MainFormApp/MainFormApp.cs
@@ -0,0 +1,32 @@
+﻿using CarRental.FormApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainFormApp
+{
+    public partial class MainFormApp : Form
+    {
+        public MainFormApp()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonForCustomerService_Click(object sender, EventArgs e)
+        {
+            CustomerForm customerForm = new CustomerForm();
+            customerForm.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
